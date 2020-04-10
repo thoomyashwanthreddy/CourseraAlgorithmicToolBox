de68f5ba7828ec4731f4dfcac6d2ffb1b19b0016
thoom <t.yashwanthreddy@gmail.com>
2020-04-10 02:29:05 -0500

    Add files via upload

---
+++ week1_programming_challenges/2_maximum_pairwise_product/MaxPairwiseProduct.java
@@ -0,0 +1,71 @@
+import java.util.*;
+import java.io.*;
+
+public class MaxPairwiseProduct {
+
+   
+
+    // faster method
+    static long getMaxPairwiseProductFast(int[] numbers) {
+        int nSize = numbers.length;
+
+        int max_index1 = -1;
+        for (int i = 0; i < nSize; i++) {
+            if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+                max_index1 = i;
+        }
+
+        int max_index2 = -1;
+        for (int j = 0; j < nSize; j++) {
+            if ((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+                max_index2 = j;
+        }
+
+        return (long)numbers[max_index1] * numbers[max_index2];
+    }
+
+    // main method
+    public static void main(String[] args) {
+        
+        FastScanner scanner = new FastScanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        
+        System.out.println(getMaxPairwiseProductFast(numbers));
+        
+
+  
+}
+    // scanning code
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner(InputStream stream) {
+            try {
+                br = new BufferedReader(new InputStreamReader(stream));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+}
